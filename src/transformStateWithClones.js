a3af80d90da0444e700724d0c6055b884b918f01
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2025-12-07 13:23:18 +0200

    done Stateful clones task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  let prevState = { ...state };
+
+  for (const action of actions) {
+    let newState;
+
+    switch (action.type) {
+      case 'addProperties':
+        newState = { ...prevState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        newState = { ...prevState };
+
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+
+      case 'clear':
+        newState = {};
+        break;
+
+      default:
+        newState = { ...prevState };
+    }
+
+    history.push(newState);
+    prevState = newState;
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
